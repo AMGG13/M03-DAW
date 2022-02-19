455dd0d9b4a3e380653725d9b2d24a186b6f05fd
AMGG13 <alvinmillergarciagarcia@gmail.com>
2022-02-19 13:27:22 +0100

    sas

---
+++ Clases y objetos/Empleado/src/objetos/Empleado.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package objetos;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Empleado {
+    private String nombre;
+    private int edad;
+    private double salario;
+
+    public Empleado(String nombre, int edad, double salario) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.salario = salario;
+    }
+    
+    public String dameDatos(){
+        return "El empleado se llama "+nombre+". Tiene "+edad+" años. Y un salario de "+salario;
+    }
+}
