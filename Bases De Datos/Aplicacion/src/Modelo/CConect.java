f9d1272865ba19924bc1de739c970c6dcd881e84
AMGG13 <alvinmillergarciagarcia@gmail.com>
2022-05-25 18:58:17 +0200

    wtf

---
+++ Bases De Datos/Aplicacion/src/Modelo/CConect.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Modelo;
+
+import com.mongodb.MongoClient;
+import java.util.List;
+
+/**
+ *
+ * @author Usuario
+ */
+public class CConect {
+    private String HOST="127.0.0.1";
+    private Integer PUERTO=27017;
+    public MongoClient Conect(){
+        MongoClient mongo = null;
+        mongo = new MongoClient(HOST,PUERTO);
+        if(mongo!=null){
+            List<String> nombreDeBD = mongo.getDatabaseNames();
+            System.out.println(nombreDeBD.toString());
+        }
+        return mongo;
+    }
+}
---
+++ Bases De Datos/Aplicacion/src/Modelo/Cuenta.java
@@ -0,0 +1,86 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Modelo;
+
+import java.util.Objects;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Cuenta {
+    private String usuario;
+    private String descripcion;
+    private int seguidores;
+
+    public Cuenta(String usuario, String descripcion, int seguidores) {
+        setUsuario(usuario);
+        setDescripcion(descripcion);
+        setSeguidores(seguidores);
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    /**
+     * Si el parametro de descripcion esta vacio, por defecto dejara la descripcion inicializada a la variable newAcc
+     * @param descripcion 
+     */
+    public void setDescripcion(String descripcion) {
+        String newAcc = "Hello, Im new using this App :)";
+        if(descripcion.isEmpty()){
+            this.descripcion = newAcc;
+        }else
+        this.descripcion = newAcc;
+    }
+
+    public void setSeguidores(int seguidores) {
+        this.seguidores = seguidores;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public int getSeguidores() {
+        return seguidores;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Cuenta other = (Cuenta) obj;
+        return Objects.equals(this.usuario, other.usuario);
+    }
+
+    @Override
+    public String toString() {
+        return "Usuario:" + usuario+" Seguidores:"+seguidores;
+    }
+    
+    
+    
+    
+    
+}
